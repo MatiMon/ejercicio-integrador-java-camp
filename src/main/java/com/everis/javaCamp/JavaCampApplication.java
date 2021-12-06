a4999c728506a21f6168d3d2e4e44b47e13fc071
mati <matimon@frba.utn.edu.ar>
2021-12-06 10:18:24 -0300

    project with hello world

---
+++ src/main/java/com/everis/javaCamp/JavaCampApplication.java
@@ -0,0 +1,13 @@
+package com.everis.javaCamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCampApplication.class, args);
+	}
+
+}
