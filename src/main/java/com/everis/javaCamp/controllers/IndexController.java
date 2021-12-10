ab251ceb63ece53db1ccdbf6eb9a51c3309e3e43
mati <matimon@frba.utn.edu.ar>
2021-12-10 09:38:48 -0300

    prueba cambio de url

--- src/main/java/com/everis/javaCamp/controllers/IndexController.java
+++ src/main/java/com/everis/javaCamp/controllers/IndexController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/api")
 public class IndexController {
 	
-	@GetMapping
+	@GetMapping("/")
 	public String index() {
 		return "home.html";
 	}
