46fe17ce95ca0c651006385c82cf0cf0fd93e6db
mati <matimon@frba.utn.edu.ar>
2021-12-10 09:48:52 -0300

    change in controller

--- src/main/java/com/everis/javaCamp/controllers/IndexController.java
+++ src/main/java/com/everis/javaCamp/controllers/IndexController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/api")
+@RequestMapping("/home")
 public class IndexController {
 	
-	@GetMapping("/")
+	@GetMapping()
 	public String index() {
 		return "home.html";
 	}
